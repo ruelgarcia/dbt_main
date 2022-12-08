643736db067449bd6e8ba165c79a85114f9459aa
william_williams2005@hotmail.com <william_williams2005@hotmail.com>
2022-12-08 03:51:12 +0000

    materialized from view to table

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,12 @@
+WITH cte_src_hosts AS (
+    SELECT 
+    host_id
+    ,NVL(host_name,'Anonymous') AS host_name
+    ,is_superhost
+    ,created_at
+    ,updated_at
+    FROM {{ref('src_hosts')}}
+)
+SELECT
+*
+FROM cte_src_hosts
---
+++ models/fct/fct_reviews.sql
@@ -0,0 +1,12 @@
+{{
+ config( materialized = 'incremental', on_schema_change='fail' )
+}}
+
+WITH cte_src_reviews AS (
+ SELECT * FROM {{ ref('src_reviews') }}
+)
+SELECT * FROM cte_src_reviews
+WHERE review_text is not null
+{% if is_incremental() %}
+ AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
